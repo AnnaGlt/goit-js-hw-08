8ef1817226cb6e61a5b41eaa3dc45ce3321071f4
AnnaGlt <Anyag@i.ua>
2022-10-26 10:05:34 +0200

    finish 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,30 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryRef = document.querySelector('.gallery');
+const cardsListMarkap = createImgCardsMarkap(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', cardsListMarkap);
+
+function createImgCardsMarkap(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__Items" href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            alt="${description}"/>
+            </a>`
+        })
+        .join('')
+};
+
+console.log(cardsListMarkap);
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250,
+ })
