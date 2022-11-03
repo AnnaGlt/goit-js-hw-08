53fffc44cd5b4f7b97c8d02a172b25462e105574
AnnaGlt <Anyag@i.ua>
2022-11-03 22:42:44 +0100

    finish 1 and procesing 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const CURRENT_TIME = "videoplayer-current-time";
+
+localStorage.setItem('CURRENT_TIME', JSON.stringify(CURRENT_TIME));
+
+updatedTime();
+
+function updatedTime() {
+    const stopTime = localStorage.getItem(CURRENT_TIME); 
+  
+    if (!stopTime) {
+        return;
+    }
+
+    const savedTime = JSON.parse((stopTime).seconds);
+   
+    player.setCurrentTime(savedTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+};
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textForm: document.querySelectorAll('.feedback-form__item'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput), 500);
+
+const STORAGE_KEY = 'feedback-form-state';
+const savedText = localStorage.getItem(STORAGE_KEY);
+
+populateTextarea();
+let formData;
+
+function onFormSubmit(evn) {
+    evn.preventDefault();
+
+    for (const element of refs.textForm) {
+        if (!element.value) {
+            window.alert("Please enter data in all fields of the form");
+            return;
+        }
+    };
+
+    if (savedText) {
+        formData = JSON.parse(savedText);
+    } else formData = {};
+
+    evn.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function onTextareaInput(evn) {
+    const massage = evn.target.value;
+
+    localStorage.setItem(STORAGE_KEY, massage);
+};
+
+function populateTextarea(evn) {
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+
+    if (saveMessage) {
+        refs.textForm.value = saveMessage;
+    }
+};
+
+refs.form.addEventListener('input', e => {
+    formData[e.target.name] = e.target.volue;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+});
